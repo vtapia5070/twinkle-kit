b0121af805ef9d85a2e0508131c2d4d6c8875801
victoria tapia <tapia.victoria5070@gmail.com>
2025-05-07 15:09:39 -0700

    add tailwindcss

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -61,6 +61,6 @@ export const WithCustomClass: Story = {
   args: {
     children: 'Custom Styled Button',
     onClick: () => {},
-    className: 'bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600',
+    className: 'some-class',
   },
 };
